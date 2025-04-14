6709511762d459af7a0103f2bec2bbf741d859f3
Aliotti Christino AILI FIDA <geko@MacBook-Pro-de-Aliotti.local>
2025-04-14 13:52:12 +0200

    chore:ios implementation

--- ios/Classes/AppManagerPlugin.swift
+++ ios/Classes/AppManagerPlugin.swift
@@ -12,6 +12,11 @@ public class AppManagerPlugin: NSObject, FlutterPlugin {
     switch call.method {
     case "getPlatformVersion":
       result("iOS " + UIDevice.current.systemVersion)
+    case "getInstalledApps":
+        // iOS does not allow querying installed apps due to privacy restrictions.
+        // Returning an empty list.
+        // let includeSystemApps = (call.arguments as? [String: Any])?["includeSystemApps"] as? Bool ?? true // Argument is received but ignored
+        result([]) // Return empty list
     default:
       result(FlutterMethodNotImplemented)
     }
